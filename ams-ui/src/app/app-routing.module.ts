09dcd3c3c25c41017a64940d14f58a9cd363d14a
Param <Paramjitsaini70@gmail.com>
2021-12-14 23:36:14 +0530

    Added topNav and sideNav
    Added topNav and sideNav

--- ams-ui/src/app/app-routing.module.ts
+++ ams-ui/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SideNavComponent } from './shared-component/side-nav/side-nav.component';
+import { TopNavComponent } from './shared-component/top-nav/top-nav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'sideNav', component: SideNavComponent },
+  { path: 'topNav', component: TopNavComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ams-ui/src/app/app.module.ts
+++ ams-ui/src/app/app.module.ts
@@ -4,17 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './component/login/login.component';
+import { SideNavComponent } from './shared-component/side-nav/side-nav.component';
+import { TopNavComponent } from './shared-component/top-nav/top-nav.component';
+import { MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SideNavComponent,
+    TopNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }

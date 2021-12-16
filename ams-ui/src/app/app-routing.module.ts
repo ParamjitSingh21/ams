9cbc56b4b18c45bd52d3c740bfae1197ae878003
Param <Paramjitsaini70@gmail.com>
2021-12-17 01:22:53 +0530

    removed topNav,sideNav routing
    removed topNav,sideNav routing

--- ams-ui/src/app/app-routing.module.ts
+++ ams-ui/src/app/app-routing.module.ts
@@ -3,12 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuardService } from './component/authguard/auth-guard.service';
 import { IndexComponent } from './component/index/index.component';
 import { LoginComponent } from './component/login/login.component';
-import { SideNavComponent } from './shared-component/side-nav/side-nav.component';
-import { TopNavComponent } from './shared-component/top-nav/top-nav.component';
 
 const routes: Routes = [
-  { path: 'sideNav', component: SideNavComponent },
-  { path: 'topNav', component: TopNavComponent },
   { path: 'login', component: LoginComponent },
   { path: '', component: IndexComponent, canActivate: [AuthGuardService] },
 ];
